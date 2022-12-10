e3772b5b74de566eb7021b4f96d206a51e4e64b9
Lefkios Paikousis (WST) <lefkiospaik@gmail.com>
2022-12-10 19:36:34 +0200

    fix bug when downloading icf

--- R/mod_download_icf.R
+++ R/mod_download_icf.R
@@ -17,7 +17,8 @@ mod_download_icf_ui <- function(id){
 }
 
 #' download_icf Server Functions
-#'
+#' @param files_download A 1 row dataframe with path_icf, lab_no and unique_id
+#' @param id internal parameter for shiny
 #' @noRd 
 mod_download_icf_server <- function(id, files_download){
   moduleServer( id, function(input, output, session){
@@ -28,21 +29,21 @@ mod_download_icf_server <- function(id, files_download){
       
       filename = function() {
         
+        # Check if it actually files_download() exists. If the user actually selected
+        # some record to download
         x <- try(files_download(), silent = TRUE)
         
         if( inherits(x, 'try-error') ){
           show_toast("error", "", "Select a Lab no fist and then click to download the ICF")
+          
         }
+        
         
         files <- strsplit(files_download()$path_icf, split = "\n") %>% unlist()
         
-        
         if(length(files) > 1){
           
-          return(
-            #paste("ICF_MultipleFiles.zip", sep = "")
-            paste0("ICF-", files_download()$lab_no, ".zip")
-          )
+          return( paste0("ICF-", files_download()$lab_no, ".zip") )
         } 
         
         if(length(files) == 1){
@@ -50,40 +51,41 @@ mod_download_icf_server <- function(id, files_download){
           if(!file.exists(file.path("ICF", files_download()$unique_id, files))){
             
             show_toast("error", "Something went wrong", "Could not find the ICF for this sample. Please contact support asap")
-            return()
+            
           }
           
-          return(
-            paste0("ICF-", files_download()$lab_no, "-", files ,".pdf")
-          )
+          return( paste0("ICF-", files_download()$lab_no, "-", files ,".pdf") )
         } 
+        
+        
+        if(length(files) == 0 ){
+          
+          show_toast("error", "Something went wrong", "Could not find the ICF for this sample. Please contact support asap")
+          
+          return("irrelevant:)")
+        }
+        
         
       },
       
       content = function(file) {
         
-        id <- showNotification(
-          "Downloading files...", 
-          duration = NULL, 
-          closeButton = FALSE
-        )
+        id <- showNotification( "Downloading files...", duration = NULL, closeButton = FALSE )
+        
         on.exit(removeNotification(id), add = TRUE)
         
         files <- strsplit(files_download()$path_icf, split = "\n") %>% unlist()
         
         if(length(files) > 1){
           
-          #files <- files_download()$path_icf
-          
           # keep only the ones that exist. In case one of the files  is not there, the zip fails
-          
           file_paths <- file.path("ICF", files_download()$unique_id, files)
           
           pdf_exist <- file.exists(file_paths) %>% setNames(file_paths) 
           
           
-          if(!all(pdf_exist)) show_toast("warning", "Something is wrong!", "Some of the ICF's you have requested, do not exist",
-                                         keepVisible = TRUE)
+          if(!all(pdf_exist)) show_toast("warning", "Something is wrong!", keepVisible = TRUE,
+                                         "Some of the ICF's you have requested, do not exist" )
           
           pdf_to_download <- pdf_exist %>% purrr::keep(isTRUE) %>% names()
           
@@ -103,18 +105,17 @@ mod_download_icf_server <- function(id, files_download){
           # keep only the ones that exist. In case one of the files  is not there, the zip fails
           pdf_exist <- file.exists(path) %>% setNames(path) 
           
-          
           if(!file.exists(path)) {
             show_toast("warning", "Something is wrong!", 
                        "The ICF for this selection does not exist", keepVisible = TRUE)
             
           }
           
-          
+          # All good, download the ICF (s)
           file.copy(path, file, overwrite = TRUE )
           
-          
         }
+        
         
       } 
       
